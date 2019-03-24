4ea48db9633e5cc2a9111fed176495a966de98a7
Mike Wong <wongmdesign@gmail.com>
2019-03-23 23:32:45 -0400

    home welcome added

--- src/components/pages/test/components/App.jsx
+++ src/components/pages/test/components/App.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, { Component } from 'react'
 import '../styles/testpage.css'
 
-const App = () => {
-  return (
-    <div className='home'>
-      <h2 id='welcome'>Welcome</h2>
-    </div>
-  )
+export default class App extends Component {
+  render () {
+    return (
+      <div id='home'>
+        <div>
+          <h1 className='home-text'>Welcome!</h1>
+          <p className='home-text'>Find all your favorite movies here!</p>
+          <p className='home-text'>
+            Find the current popular movies, top rated movies and upcoming
+            movies.
+          </p>
+          <p className='home-text'>
+            Browse through the movie titles, release dates, images, and
+            descriptions.
+          </p>
+          <p className='home-text'>Enjoy!</p>
+        </div>
+      </div>
+    )
+  }
 }
-
-export default App
