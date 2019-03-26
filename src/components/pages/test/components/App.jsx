de7144a200f49479eeace46329824d43c79a8e82
Mike Wong <wongmdesign@gmail.com>
2019-03-26 10:25:38 -0400

    fixed functions where it should be stateless

--- src/components/pages/test/components/App.jsx
+++ src/components/pages/test/components/App.jsx
@@ -1,24 +1,23 @@
-import React, { Component } from 'react'
+import React from 'react'
 import '../styles/testpage.css'
 
-export default class App extends Component {
-  render () {
-    return (
-      <div id='home'>
-        <div>
-          <h1 className='home-text'>Welcome!</h1>
-          <p className='home-text'>Find all your favorite movies here!</p>
-          <p className='home-text'>
-            Find the current popular movies, top rated movies and upcoming
-            movies.
-          </p>
-          <p className='home-text'>
-            Browse through the movie titles, release dates, images, and
-            descriptions.
-          </p>
-          <p className='home-text'>Enjoy!</p>
-        </div>
+const App = () => {
+  return (
+    <div id='home'>
+      <div>
+        <h1 className='home-text'>Welcome!</h1>
+        <p className='home-text'>Find all your favorite movies here!</p>
+        <p className='home-text'>
+          Find the current popular movies, top rated movies and upcoming movies.
+        </p>
+        <p className='home-text'>
+          Browse through the movie titles, release dates, images, and
+          descriptions.
+        </p>
+        <p className='home-text'>Enjoy!</p>
       </div>
-    )
-  }
+    </div>
+  )
 }
+
+export default App
--- src/components/pages/test/components/main/MovieCard.jsx
+++ src/components/pages/test/components/main/MovieCard.jsx
@@ -1,48 +1,47 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
 import { thumbnailBaseURL } from '../../api/theMovieDB'
 
-export default class MovieCard extends Component {
-  render () {
-    const { id, title, poster_path, release_date } = this.props.movie
+const MovieCard = props => {
+  const { id, title, poster_path, release_date } = props.movie
 
-    return (
-      <div>
-        {/* Render only movies that have images and release date */}
-        {poster_path && release_date && (
-          <div>
-            {/* wrap titles if they are too long */}
-            {title.length > 25 ? (
-              <div>
-                <p className='text title'>
-                  {title
-                    .split(' ')
-                    .slice(0, 4)
-                    .join(' ')}
-                </p>
-                <p className='text title'>
-                  {title
-                    .split(' ')
-                    .slice(4)
-                    .join(' ')}
-                </p>
-              </div>
-            ) : (
-              <p className='text title'>{title}</p>
-            )}
-            {/* <p className='text'>{title}</p> */}
-            <p className='text released'>Released:</p>
-            <p className='text date'>{release_date}</p>
-            <Link to={`/movie/${id}`}>
-              <img
-                className='movie-card'
-                alt={title}
-                src={`${thumbnailBaseURL}${poster_path}`}
-              />
-            </Link>
-          </div>
-        )}
-      </div>
-    )
-  }
+  return (
+    <div>
+      {/* Render only movies that have images and release date */}
+      {poster_path && release_date && (
+        <div>
+          {/* wrap titles if they are too long */}
+          {title.length > 25 ? (
+            <div>
+              <p className='text title'>
+                {title
+                  .split(' ')
+                  .slice(0, 4)
+                  .join(' ')}
+              </p>
+              <p className='text title'>
+                {title
+                  .split(' ')
+                  .slice(4)
+                  .join(' ')}
+              </p>
+            </div>
+          ) : (
+            <p className='text title'>{title}</p>
+          )}
+          <p className='text released'>Released:</p>
+          <p className='text date'>{release_date}</p>
+          <Link to={`/movie/${id}`}>
+            <img
+              className='movie-card'
+              alt={title}
+              src={`${thumbnailBaseURL}${poster_path}`}
+            />
+          </Link>
+        </div>
+      )}
+    </div>
+  )
 }
+
+export default MovieCard
