2c87ff1691683e890148c3e6cb5b75f78f513dcb
Mike Wong <wongmdesign@gmail.com>
2019-06-12 16:10:05 -0400

    updated home to search

--- src/components/navigation/Navbar.jsx
+++ src/components/navigation/Navbar.jsx
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div className='navbar'>
-      <Link to='/'>Home</Link>
-      <Link to='/popular'>Popular</Link>
-      <Link to='/upcoming'>Upcoming</Link>
-      <Link to='/toprated'>Top Rated</Link>
+    <div className="navbar">
+      <Link to="/">Search</Link>
+      <Link to="/popular">Popular</Link>
+      <Link to="/upcoming">Upcoming</Link>
+      <Link to="/toprated">Top Rated</Link>
     </div>
   )
 }
