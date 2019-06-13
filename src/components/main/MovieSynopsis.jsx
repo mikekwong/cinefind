4d2f5b62116c0fc3091829a06a02ac23f4f65b07
Mike Wong <wongmdesign@gmail.com>
2019-06-13 15:49:40 -0400

    fixed hr issue

--- src/components/main/MovieSynopsis.jsx
+++ src/components/main/MovieSynopsis.jsx
@@ -2,11 +2,16 @@ import React from 'react'
 
 export default ({ title, releaseDate, overview }) => {
   return (
-    <div className='movie-detail-info'>
-      <p className='text title'>{title}</p>
-      {title && <p className='text released'>Released:</p>}
-      <p className='text date'>{releaseDate}</p>
-      <p className='text description-text overview'>{overview}</p>
+    <div className="movie-detail-info">
+      <p className="text title">{title}</p>
+      {title && (
+        <>
+          <hr style={{ width: '100%' }} />
+          <p className="text released">Released:</p>
+        </>
+      )}
+      <p className="text date">{releaseDate}</p>
+      <p className="text description-text overview">{overview}</p>
     </div>
   )
 }
